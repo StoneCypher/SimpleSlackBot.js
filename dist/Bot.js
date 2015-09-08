4539fa99fe48f5a21e361858bb2beb6f4870a0e9
John Haugeland <stonecypher@gmail.com>
2015-09-08 14:51:54 -0700

    working babel process

---
+++ dist/Bot.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+var _default = function _default() {
+  _classCallCheck(this, _default);
+};
+
+exports['default'] = _default;
+module.exports = exports['default'];
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,29 @@
 
-var gulp = require('gulp');
+var gulp  = require('gulp'),
 
-var dirs = require('./config/dirs.json');
+    babel = require('gulp-babel');
 
-gulp.task('default', []);
+
+
+var dirs      = require('./config/dirs.json'),
+    targets   = require('./config/targets.json'),
+    babel_cfg = require('./config/babel.json');
+
+
+
+
+
+gulp.task('babel', function() {
+
+  return gulp.src(targets.babel)
+    .pipe(babel(babel_cfg))
+    .pipe(gulp.dest(dirs.dist));
+
+});
+
+
+
+
+
+gulp.task('build',   ['babel']);
+gulp.task('default', ['build']);
