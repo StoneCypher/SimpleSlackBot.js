203dd7e0f118011b7038cfb6f74c47eb4dd2762e
John Haugeland <stonecypher@gmail.com>
2015-09-08 17:10:37 -0700

    basic connect cycle

--- dist/Bot.js
+++ dist/Bot.js
@@ -5,11 +5,63 @@ Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-var _default = function _default() {
-  _classCallCheck(this, _default);
-};
+var slack_lib = require('slack-client'),
+    util = require('util');
+
+var _default = (function () {
+  function _default(options) {
+    _classCallCheck(this, _default);
+
+    if (options.key.indexOf('xoxb') !== 0) {
+      throw 'All valid Slack bot keys start \'xoxb\'.  Verify the `key` in your `options`.';
+    }
+
+    this.key = options.key;
+    this.handlers = options.handlers || []; // whargarbl create default handler
+    this.autoReconnect = options.autoReconnect || true;
+    this.autoMark = options.autoMark || true;
+
+    this.slack = new slack_lib(this.key, this.autoReconnect, this.autoMark);
+  }
+
+  _createClass(_default, [{
+    key: 'handleConnect',
+    value: function handleConnect() {
+      /*
+          var channels = myChannels(),
+              groups   = myGroups();
+      */
+      console.log('Welcome to Slack. You are ' + this.slack.self.name + ' of ' + slack.team.name);
+      /*
+          console.log( (channels.length > 0)?
+            ('You are in channels #' + channels.join(', #')) :
+            ('You are not in any channels.'                ));
+      
+          console.log( (groups.length > 0)?
+            ('You are in groups %' + groups.join(', %')) :
+            ('You are not in any groups.'              ));
+      */
+    }
+  }, {
+    key: 'handleMessage',
+    value: function handleMessage(msg) {}
+  }, {
+    key: 'connect',
+    value: function connect() {
+
+      this.slack.on('open', this.handleConnect);
+      this.slack.on('message', this.handleMessage);
+
+      this.slack.login();
+    }
+  }]);
+
+  return _default;
+})();
 
 exports['default'] = _default;
 module.exports = exports['default'];
--- src/Bot.js
+++ src/Bot.js
@@ -1,6 +1,65 @@
 
 'use strict';
 
+var slack_lib = require('slack-client'),
+    util      = require('util');
+
+
+
+
+
 export default class {
+
+  constructor(options) {
+
+    if (options.key.indexOf('xoxb') !== 0) {
+      throw 'All valid Slack bot keys start \'xoxb\'.  Verify the `key` in your `options`.';
+    }
+
+    this.key           = options.key;
+    this.handlers      = options.handlers      || []; // whargarbl create default handler
+    this.autoReconnect = options.autoReconnect || true;
+    this.autoMark      = options.autoMark      || true;
+
+    this.slack         = new slack_lib(this.key, this.autoReconnect, this.autoMark);
+
+  }
+
+
+
+  handleConnect() {
+/*
+    var channels = myChannels(),
+        groups   = myGroups();
+*/
+    console.log('Welcome to Slack. You are ' + this.slack.self.name
+              + ' of '                       + slack.team.name);
+/*
+    console.log( (channels.length > 0)?
+      ('You are in channels #' + channels.join(', #')) :
+      ('You are not in any channels.'                ));
+
+    console.log( (groups.length > 0)?
+      ('You are in groups %' + groups.join(', %')) :
+      ('You are not in any groups.'              ));
+*/
+  }
+
+
+
+  handleMessage(msg) {
+
+  }
+
+
+
+  connect() {
+
+    this.slack.on('open',    this.handleConnect);
+    this.slack.on('message', this.handleMessage);
+
+    this.slack.login();
+
+  }
 
 }
